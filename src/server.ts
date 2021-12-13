1dbac5452b45c9df7ff9f2e7b42f089b0f93c98c
Abnet Nega <86370531+AbBiz101@users.noreply.github.com>
2021-12-13 22:22:03 +0100

    test

--- src/server.ts
+++ src/server.ts
@@ -1,44 +1,32 @@
 import cors from 'cors';
-import Endpoints from 'express-list-endpoints';
-import mongoose from 'mongoose';
 import { app } from './app';
+import mongoose from 'mongoose';
+import Endpoints from 'express-list-endpoints';
 
 process.env.TS_NODE_DEV && require('dotenv').config();
 
-
-
 /* *************************** CORS middleware  ************************************* */
-// const whiteList = [process.env.FE_LOCAL_URL, process.env.FE_PROD_URL];
-// const corsOpts = {
-// 	origin: function (origin: any, next: any) {
-// 		if (!origin || whiteList.indexOf(origin) !== -1) {
-// 			next(null, true);
-// 		} else {
-// 			next(new Error());
-// 		}
-// 	},
-// };
-// server.use(cors(corsOpts));
+const whiteList = [process.env.FE_LOCAL_URL!, process.env.FE_PROD_URL!];
 
-
-
+const corsOpts = {
+	origin: function (origin: any, next: any) {
+		if (!origin || whiteList.indexOf(origin) !== -1) {
+			next(null, true);
+		} else {
+			next(new Error());
+		}
+	},
+};
+app.use(cors(corsOpts));
 
 /* *************************** Starting the sever  ************************************* */
 const port = process.env.PORT;
 
-app.listen(port, async () => {
-	try {
-		mongoose.connect(process.env.MONGO_CONNECTION!, {
-			//  useNewUrlParser: true,
-			//  useUnifiedTopology: true,
-		});
-		console.log(`✅ Server is running on ${port}  and connected to db`);
-	} catch (error) {
-		console.log('Db connection is failed ', error);
-	}
+mongoose.connect(process.env.MONGO_CONNECTION!).then(() => {
+	console.log(`Connected to Mongo`);
+	app.listen(port, () => {
+		console.log(`Server is running on port ${port}`);
+	});
 });
 
-app.on('error', (error) =>
-	console.log(`❌ Server is not running due to : ${error}`),
-);
 console.table(Endpoints(app));
